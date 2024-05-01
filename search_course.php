f899b0b9666e2af41bc3d91fc8ab19f64a195dc1
SlicedYams <MDRockets01@gmail.com>
2024-05-01 18:13:02 -0500

    marks edits

--- search_course.php
+++ search_course.php
@@ -6,27 +6,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <!--From Bootstrap documentation: Create a new index.html file in your project root. Include the <meta name="viewport"> tag as well for proper responsive behavior in mobile devices.-->
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>AcaStat - Home Page</title>
+  <title>AcaStat</title>
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
   <!--Style sheet for icons: https://icons.getbootstrap.com/-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="css/home.css">
-
+  <link rel="stylesheet" href="css/course.css">
   <?php
     session_start();
     if (!isset($_SESSION["loggedIn"])) {
       include('php/login_check.php');
       shell_exec('php login_check.php');
     }
-    
-    //connect database
+
     $conn = mysqli_connect('database-1.cs1hkdhivv1o.eu-central-1.rds.amazonaws.com', 'admin', 'JtKRAYtPsXWUU8fYQNdf', 'acastat-database');
     if($conn->connect_error) {
         die('Connection Failed : '.$conn->connect_error);
     }
-      
   ?>
-
 </head>
 
 <body>
@@ -44,11 +40,6 @@
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Search Catalogue</a>
-            </li>
-
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Institution</a>
               <ul class="dropdown-menu">
@@ -68,10 +59,9 @@
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" href="#">Separated link</a></li>
               </ul>
-            </li>    
+            </li>     
           </ul>
         </div>
-
       </div>
     </nav>
   </header>
@@ -80,96 +70,103 @@
     <!--For icons: https://icons.getbootstrap.com/-->
     <!--For button link: https://stackoverflow.com/questions/36003670/how-to-put-a-link-on-a-button-with-bootstrap-->
 
-    <!--Home Page Banner-->
-    <div class="jumbotron" >
-      <h1 id="banner-text" class="display-4">Welcome back,
-      <?php 
-        $sql = "SELECT firstName, lastName FROM users WHERE username ='" . $_SESSION['username'] . "'";
+    <form class="needs-validation" action="php/create-course-handler.php" method="post" novalidate>
 
-        $result = mysqli_query($conn, $sql);
+<h1 class = "text-center">Search Catalogue</h1>
 
-        $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
+<hr></hr>
 
-          echo $user[0]['firstName'] . " " . $user[0]['lastName'];
-      ?>
+<div class="row justify-content-center">
+    <div class="col-sm-2">
+        <label for="state" class="col-sm-2 control-label">Subject</label>
+        <select class="form-control" id="subject" name="subject">
+            <option value="">N/A</option>
+            <option value="ACCT">Accounting</option>
+            <option value="ARTS">Art</option>
+            <option value="BIOL">Biology</option>
+            <option value="CHEM">Chemistry</option>
+            <option value="COMM">Communication</option>
+            <option value="COMP">Computer Science</option>
+            <option value="ECON">Economics</option>
+            <option value="ENGL">English</option>
+            <option value="GEOG">Geograhy</option>
+            <option value="HIST">History</option>
+            <option value="MATH">Math</option>
+            <option value="MUSC">Msuic</option>
+            <option value="PHYS">Physics</option>
+            <option value="PSYC">Physcology</option>
+            <option value="SOCI">Sociology</option>
+        </select>
     </div>
-    
-    <div class="tool-bar">
-      <a class="tool-button" role="button" href="calculator.php">Planner <i class="bi bi-calculator"></i></a>
-      <a class="tool-button" role="button" href="planner.php">Calender <i class="bi bi-calendar3"></i></a>
+
+
+  <div class="col-sm-2">
+    <label for="validationTooltip08">Course Code</label>
+    <input type="text" class="form-control" id="validationTooltip08" placeholder="ex. 244" name="code" required>
+    <div class="valid-tooltip">
+      Looks good!
     </div>
-  
-      <div id="card-div">
-        <a href="">
-          <div class="card">
-            <img src="photos/card-photo-1.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024</p>
-                <p class="card-text">BIO 123: Introduction to Biology</p>
-              </div>
-          </div>
-        </a>
+  </div>
 
-        <a href=""> 
-          <div class="card">
-            <img src="photos/card-photo-2.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024 </p>
-                <p class="card-text">BIO 444: Molecular Biology</p>
-              </div>
-          </div>
-        </a>
 
-        <a href="">
-          <div class="card">
-            <img src="photos/card-photo-3.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024</p>
-                <p class="card-text">SOC 143: Humanities I</p>
-              </div>
-          </div>
-        </a>
+  <div class="col-sm-2">
+    <label for="state" class="col-sm-2 control-label">Semester</label>
+    <select class="form-control" id="semester" name="semester">
+      <option value="">N/A</option>
+      <option value="Fall">Fall</option>
+      <option value="Spring">Spring</option>
+      <option value="Summer">Summer</option>
+    </select>
+  </div>
 
-        <a href="">
-          <div class="card">
-            <img src="photos/card-photo-4.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024</p>
-                <p class="card-text">PYSC 423: Astrophysics</p>
-              </div>
-          </div>
-        </a>
+  <div class="col-sm-2">
+    <label for="state" class="col-sm-2 control-label">Year</label>
+    <select class="form-control" id="year" name="year">
+      <option value="">N/A</option>
+      <option value="2024">2024</option>
+      <option value="2025">2025</option>
+      <option value="2026">2026</option>
+      <option value="2027">2027</option>
+      <option value="2028">2028</option>
+    </select>
+  </div>
 
-        <a href="">
-          <div class="card">
-            <img src="photos/card-photo-5.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024</p>
-                <p class="card-text">BIO 123: Introduction to Biology</p>
-              </div>
-          </div>
-        </a>
+  <div class="col-sm-2">
+    <label for="state" class="col-sm-2 control-label">Instructor</label>
+    <select class="form-control" id="instructor" name="instructor">
+      <option value="">N/A</option>
+      <?php
+          $sql = "SELECT firstName, lastName, instructors.userID FROM instructors INNER JOIN users ON users.userID = instructors.userID ORDER BY lastName";
 
-        <a href="">
-          <div class="card">
-            <img src="photos/card-photo-6.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <p class="card-university">Winona State University</p>
-                <p class="section">Spring 2024</p>
-                <p class="card-text">BIO 444: Molecular Biology</p>
-              </div>
-          </div>
-        </a>
-      </div>
-  </main>
+          $result = mysqli_query($conn, $sql);
+
+          $instructors = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+          foreach($instructors as $row) {
+            echo "<option value='" . $row["userID"] . "'>" . $row["lastName"] . ", " . $row["firstName"] . "</option>";
+          } 
+        ?>
+    </select>
+  </div>
+</div>
+
+<hr></hr>
+    <div class="text-center">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+    </div>
+</form>
+
+<hr></hr>
+
+    <div class="course-card">
+          <h1>Subject Code: Course Title</h1>
+          <p>Semester Year</p>
+    </div>
+
   <footer>
       <p class="copyright">Copyright 2024-2024 by NiddyGriddy. All Rights Reserved. AcaStat is Powered by yours truly.</p>
   </footer>
+
   <!--From Bootstrap documentation: Include Bootstrap’s CSS and JS. Place the <link> tag in the <head> for our CSS, and the <script> tag for our JavaScript bundle (including Popper for positioning dropdowns, poppers, and tooltips) before the closing </body>. Learn more about our CDN links.-->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
