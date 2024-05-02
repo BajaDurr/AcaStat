6f9333702f604bdbc3625099398b7c423683abac
krish7201 <44722635+krish7201@users.noreply.github.com>
2024-05-01 19:33:54 -0500

    footer, head, navbar as php components

--- course.php
+++ course.php
@@ -1,85 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!--From Bootstrap documentation: Create a new index.html file in your project root. Include the <meta name="viewport"> tag as well for proper responsive behavior in mobile devices.-->
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>AcaStat</title>
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-  <!--Style sheet for icons: https://icons.getbootstrap.com/-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="css/course.css">
-  <?php
-    session_start();
-    if (!isset($_SESSION["loggedIn"])) {
-      include('php/login_check.php');
-      shell_exec('php login_check.php');
-    }
-  ?>
-</head>
-
+<head><?php include "php/head.php"; exec("php php/head.php");?></head>
 <body>
-
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <img class="mb-4" src="assets/CAP_LOGO.png" alt="" width="5%" height=auto></img>
-        <a class="navbar-brand" href="#">AcaStat</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Institution</a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Separated link</a></li>
-              </ul>
-            </li>    
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Courses</a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Separated link</a></li>
-              </ul>
-            </li>
-            <?php
-              $con = mysqli_connect('database-1.cs1hkdhivv1o.eu-central-1.rds.amazonaws.com', 'admin', 'JtKRAYtPsXWUU8fYQNdf', 'acastat-database');
-
-              if ($con) {
-                $uname = $_SESSION["username"];
-
-                $sql = "SELECT * FROM users INNER JOIN admins WHERE username = '$uname' AND users.userID = admins.userID";
-
-                $result = mysqli_query($con, $sql);
-                if (mysqli_num_rows($result) == 1) {
-                  echo "<li class='nav-item dropdown'>";
-                  echo "<a class='nav-link dropdown-toggle' data-bs-toggle='dropdown' href='#' role='button' aria-expanded='false'>Admin Tools</a>";
-                  echo "<ul class='dropdown-menu'>";
-                  echo "<li><a class='dropdown-item' href='create_user.php'>Create New User</a></li>";
-                  echo "<li><a class='dropdown-item' href='create_course.php'>Create New Course</a></li>";
-                  echo "</ul>";
-                  echo "</li>";
-                }
-              } 
-            ?>     
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+  <header><?php include "php/navbar.php"; exec("php php/navbar.php");?></header>
 
   <main>
     <!--For icons: https://icons.getbootstrap.com/-->
@@ -149,12 +72,7 @@
     </div>
   </main>
 
-  <footer>
-      <p class="copyright">Copyright 2024-2024 by NiddyGriddy. All Rights Reserved. AcaStat is Powered by yours truly.</p>
-  </footer>
+  <footer class="mt-5 text-center"><?php include "php/footer.php"; exec("php php/footer.php");?></footer>
 
-  <!--From Bootstrap documentation: Include Bootstrap’s CSS and JS. Place the <link> tag in the <head> for our CSS, and the <script> tag for our JavaScript bundle (including Popper for positioning dropdowns, poppers, and tooltips) before the closing </body>. Learn more about our CDN links.-->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
--- planner.php
+++ planner.php
@@ -1,84 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AcaStat</title>
-    <link rel="icon" type="assets/CAP_LOGO" href="/assets/CAP_LOGO.png">
-    
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/calendar.css" rel="stylesheet">
-
-    <?php
-        session_start();
-        if (!isset($_SESSION["loggedIn"])) {
-            include('php/login_check.php');
-            shell_exec('php login_check.php');
-        }
-    ?>
-</head>
+<head><?php include "php/head.php"; exec("php php/head.php");?></head>
 <body>
-    <div class="container">
-        <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <img class="mb-4" src="assets/CAP_LOGO.png" alt="" width="5%" height="auto"></img>
-                    <a class="navbar-brand" href="home.php">AcaStat</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Institution</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                                </ul>
-                            </li>    
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Courses</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                                </ul>
-                            </li>  
-                            <?php
-                              $con = mysqli_connect('database-1.cs1hkdhivv1o.eu-central-1.rds.amazonaws.com', 'admin', 'JtKRAYtPsXWUU8fYQNdf', 'acastat-database');
-
-                              if ($con) {
-                                $uname = $_SESSION["username"];
-
-                                $sql = "SELECT * FROM users INNER JOIN admins WHERE username = '$uname' AND users.userID = admins.userID";
-
-                                $result = mysqli_query($con, $sql);
-                                if (mysqli_num_rows($result) == 1) {
-                                  echo "<li class='nav-item dropdown'>";
-                                  echo "<a class='nav-link dropdown-toggle' data-bs-toggle='dropdown' href='#' role='button' aria-expanded='false'>Admin Tools</a>";
-                                  echo "<ul class='dropdown-menu'>";
-                                  echo "<li><a class='dropdown-item' href='create_user.php'>Create New User</a></li>";
-                                  echo "<li><a class='dropdown-item' href='create_course.php'>Create New Course</a></li>";
-                                  echo "</ul>";
-                                  echo "</li>";
-                                }
-                              } 
-                            ?>   
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+  <header><?php include "php/navbar.php"; exec("php php/navbar.php");?></header>
 
         <main>
             <div class="row mt-4">
@@ -129,10 +53,10 @@
         </main>
     </div>
 
-    <!--From Bootstrap documentation: Include Bootstrap’s CSS and JS. Place the <link> tag in the <head> for our CSS, and the <script> tag for our JavaScript bundle (including Popper for positioning dropdowns, poppers, and tooltips) before the closing </body>. Learn more about our CDN links.-->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
     <!-- Custom JavaScript -->
     <script src="js/calendar.js"></script>
+
+    <footer class="mt-5 text-center"><?php include "php/footer.php"; exec("php php/footer.php");?></footer>
 </body>
 </html>
