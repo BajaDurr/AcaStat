8c7dacfd9765b769ae50cf8d9bb119a8d1ce4289
David Krotzer <bajablastingamers@gmail.com>
2024-05-02 07:08:11 -0500

    Favicon

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>AcaStat</title>
+	<link rel = "icon" type= "assets/CAP_LOGO.png" href=/assets/CAP_LOGO.png>
 	<!-- Bootstrap core CSS -->
 	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 	<!-- Custom styles for this template -->
--- php/head.php
+++ php/head.php
@@ -4,6 +4,7 @@ echo '<meta http-equiv="X-UA-Compatible" content="IE=edge" />';
 echo '<!--From Bootstrap documentation: Create a new index.html file in your project root. Include the <meta name="viewport"> tag as well for proper responsive behavior in mobile devices.-->';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1" />';
 echo '<title>AcaStat</title>';
+echo '<link rel = "icon" type= "assets/CAP_LOGO.png" href=/assets/CAP_LOGO.png>';
 echo '<!-- Bootstrap core CSS -->';
 echo '<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"/>';
 session_start();
